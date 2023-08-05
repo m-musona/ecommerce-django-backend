73a8396883d42447f806714e93d1461da401cddd
Mutale Musona <musonamutale@gmail.com>
2023-08-05 18:52:42 +0200

    Added django-cors-header Support

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "products",
     "pages",
     "rest_framework",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
